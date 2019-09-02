44e62c753c1b30dd551f15b125872dfcbf4844bb
Nelson Veras <nveras@techcommwireless.com>
2019-09-02 15:51:30 -0400

    Added Account controller Added Login action Added Login View

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
