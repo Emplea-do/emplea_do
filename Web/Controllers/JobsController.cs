1ddffe8d4a67730c0bf4bc0ccf9dca7c067534b5
César Lachapelle <cesarlachapelle05@gmail.com>
2019-10-14 01:37:49 -0400

    fix typo

--- Web/Controllers/JobsController.cs
+++ Web/Controllers/JobsController.cs
@@ -54,7 +54,7 @@ namespace Web.Controllers
             if (ModelState.IsValid)
             {
                 var url = $"{this.Request.Scheme}://{this.Request.Host}/";
-                System.Threading.Tasks.Task.Run(()=>_twitterService.Tweet($"Se busca: {model.Title} para mas informacion dirigirse emplea.do {url} "));
+                System.Threading.Tasks.Task.Run(()=>_twitterService.Tweet($"Se busca: {model.Title} para mas información, dirigirse a emplea.do {url} "));
 
                 return RedirectToAction("", "");
             }
