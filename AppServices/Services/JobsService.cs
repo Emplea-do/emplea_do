99a65db9695e1b0d3c75529e079ce70cc0c1d0c8
Claudio Sanchez <claudio@megsoftconsulting.com>
2019-11-03 12:19:51 -0500

    Made changes to use Azure App Configuration and feature flags

--- AppServices/Services/JobsService.cs
+++ AppServices/Services/JobsService.cs
@@ -5,13 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using AppServices.Services;
 using AppServices.Framework;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace AppServices.Services
 {
     public class JobsService : BaseService<Job, IJobsRepository>, IJobsService
     {
-        public JobsService(IJobsRepository jobsRepository) : base(jobsRepository)
+        private readonly IConfiguration _config;
+
+        public JobsService(IJobsRepository jobsRepository, IConfiguration config) : base(jobsRepository)
         {
+            this._config = config;
         }
 
         protected override TaskResult<Job> ValidateOnCreate(Job entity)
@@ -28,127 +33,9 @@ namespace AppServices.Services
         {
             return new TaskResult<Job>();
         }
-        /*
-public override List<Job> GetAll()
-{
-   return _jobsRepository.GetAll().ToList();
-   /*
-   return new List<Job>
-   {
-       new Job
-       {
-           Id=1,
-           Title = "Trabajo de prueba",
-           Description="Esto es un lorem ipsum",
-           HowToApply="Para aplicar mandame un correo plz",
-           Approved=true,
-           Company= new Company
-           {
-               Url="https://megsoftconsulting.com/",
-               LogoUrl = "https://localhost:5001/img/logo.png"
-           }
-       },
-       new Job
-       {
-           Id=2,
-           Title = "Trabajo de prueba 2",
-           Description="Esto es un lorem ipsum",
-           HowToApply="Para aplicar mandame un correo plz",
-           Approved=true,
-           Company= new Company
-           {
-               Url="https://megsoftconsulting.com/",
-               LogoUrl = "https://localhost:5001/img/logo.png"
-           }
-       },
-       new Job
-       {
-           Id=14,
-           Title = "Trabajo de prueba 4",
-           Description="Esto es un lorem ipsum",
-           HowToApply="Para aplicar mandame un correo plz",
-
-           Company= new Company
-           {
-               Url="https://megsoftconsulting.com/",
-               LogoUrl = "https://localhost:5001/img/logo.png"
-           }
-       },
-       new Job
-       {
-           Id=7,
-           Title = "Trabajo de prueba 55",
-           Description="Esto es un lorem ipsum",
-           HowToApply="Para aplicar mandame un correo plz",
-           UserId=10,
-           Approved = false,
-           Company= new Company
-           {
-               Url="https://megsoftconsulting.com/",
-               LogoUrl = "https://localhost:5001/img/logo.png"
-           },
-       },
-       new Job
-       {
-           Id=16,
-           Title = "Trabajo de prueba",
-           Description="Esto es un lorem ipsum",
-           HowToApply="Para aplicar mandame un correo plz",
-
-           Company= new Company
-           {
-               Url="https://megsoftconsulting.com/",
-               LogoUrl = "https://localhost:5001/img/logo.png"
-           }
-       },
-   };
-    }*/
-
         public List<Job> GetByUser(int userId)
         {
             return _mainRepository.Get(x=>x.UserId == userId).OrderByDescending(x=>x.PublishedDate).ToList();
-           /* return new List<Job>
-            {
-                new Job
-                {
-                    Id=1,
-                    Title = "Trabajo de prueba",
-                    Description="Esto es un lorem ipsum",
-                    HowToApply="Para aplicar mandame un correo plz",
-
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-                new Job
-                {
-                    Id=2,
-                    Title = "Trabajo de prueba 2",
-                    Description="Esto es un lorem ipsum",
-                    HowToApply="Para aplicar mandame un correo plz",
-
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-                new Job
-                {
-                    Id=14,
-                    Title = "Trabajo de prueba 4",
-                    Description="Esto es un lorem ipsum",
-                    HowToApply="Para aplicar mandame un correo plz",
-
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-            };*/
         }
 
         public Job GetDetails(int id, bool isPreview = false)
@@ -167,49 +54,6 @@ public override List<Job> GetAll()
         public IEnumerable<Job> GetRecentJobs()
         {
             return _mainRepository.Get(x=>x.IsActive && !x.IsHidden, "Company").OrderByDescending(x => x.PublishedDate).Take(10).ToList();
-            /*
-            List<Job> jobsList = new List<Job>
-            {
-                new Job
-                {
-                    Id=1,
-                    Title = "No hay trabajo",
-                    Description="Esta no es una descripción para un trabajo",
-                    HowToApply="No apliquen por favor",
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-                new Job
-                {
-                    Id=2,
-                    Title = "Trabajo de prueba 2",
-                    Description="Esto es un lorem ipsum",
-                    HowToApply="Para aplicar mandame un correo plz",
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-                new Job
-                {
-                    Id=14,
-                    Title = "Trabajo de prueba 4",
-                    Description="Esto es un lorem ipsum",
-                    HowToApply="Para aplicar mandame un correo plz",
-
-                    Company= new Company
-                    {
-                        Url="https://megsoftconsulting.com/",
-                        LogoUrl = "https://localhost:5001/img/logo.png"
-                    }
-                },
-            };
-            var recentJobs = jobsList.OrderByDescending(x => x.CreatedAt).Take(10);
-            return recentJobs;*/
         }
     }
 
