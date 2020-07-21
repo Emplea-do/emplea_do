5613108d42156cfb0b97bedf2726cde40eebc5b7
Eddy Cordero <eddycordero85@gmail.com>
2020-07-20 21:35:41 -0400

    Adding banners entity

--- Migrations/Scripts/up.sh
+++ Migrations/Scripts/up.sh
@@ -1,3 +1,3 @@
 ﻿dotnet restore ../Migrations.csproj
 dotnet build ../Migrations.csproj
-dotnet fm migrate -p sqlite -c "Data Source=../../mydb.db" -a "../bin/Debug/netcoreapp2.2/Migrations.dll"
+dotnet fm migrate -p sqlite -c "Data Source=../../mydb.db" -a "../bin/Debug/netcoreapp3.1/Migrations.dll"
