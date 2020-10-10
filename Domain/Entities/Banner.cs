36f8b301aaa73461566ab54b72c271d681382178
Eddy Cordero <eddycordero85@gmail.com>
2020-10-10 08:42:26 -0400

    Add spaces to banner

--- Domain/Entities/Banner.cs
+++ Domain/Entities/Banner.cs
@@ -3,9 +3,13 @@ namespace Domain.Entities
     public class Banner : Entity
     {
         public int UserId { get; set; }
+
         public virtual User User { get; set; }
+
         public string ImageUrl { get; set; }
+
         public string DestinationUrl { get; set; }
+
         public bool IsApproved { get; set; }
     }
 }
