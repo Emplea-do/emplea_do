ee7c97cdebe57efc18160710d4bb03c3670f788c
Robert Lluberes <robertlluberes@gmail.com>
2022-11-21 09:29:26 -0400

    Bugfix/240 rename wizard view for Company and Banner (#241)
    * Rename the view Wizard to Create for BannersController
    
    * Rename the view Wizard to Create for CompaniesController
    
    * Fix LogoUrl self-assignment in CompaniesController

--- Web/Controllers/BannersController.cs
+++ Web/Controllers/BannersController.cs
@@ -1,33 +1,40 @@
-using AppServices.Services;
 using AppServices.Framework;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Web.Framework.Helpers.Alerts;
-using Web.ViewModels;
-using System;
-using System.IO;
-using Microsoft.AspNetCore.Http;
+using AppServices.Services;
 using Domain.Entities;
 using ElmahCore;
-using Web.Services.Slack;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Net;
-using Web.Models.Slack;
-using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Web.Framework.Helpers.Alerts;
+using Web.Models.Slack;
+using Web.Services.Slack;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class BannersController : BaseController
     {
         private readonly IBannersService _bannersService;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly ISlackService _slackService;
-         private readonly IConfiguration _configuration;
 
-        public  BannersController(IBannersService bannersService, IWebHostEnvironment webHostEnvironment, ISlackService slackService, IConfiguration configuration)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        private readonly ISlackService _slackService;
+
+        private readonly IConfiguration _configuration;
+
+        public BannersController(
+            IBannersService bannersService,
+            IWebHostEnvironment webHostEnvironment,
+            ISlackService slackService,
+            IConfiguration configuration)
         {
             _bannersService = bannersService;
             _webHostEnvironment = webHostEnvironment;
@@ -37,12 +44,12 @@ namespace Web.Controllers
 
         public IActionResult Index()
         {
-            var companies = _bannersService.GetAll();
-            return View(companies);
+            var banners = _bannersService.GetAll();
+            return View(banners);
         }
-        
+
         [Authorize]
-        public IActionResult Wizard(int? id)
+        public IActionResult Create(int? id)
         {
             var model = new BannerViewModel();
 
@@ -50,7 +57,7 @@ namespace Web.Controllers
             {
                 var banner = _bannersService.GetById(id.Value);
 
-                if(banner.UserId == _currentUser.UserId)
+                if (banner.UserId == _currentUser.UserId)
                 {
                     model.Id = banner.Id;
                     model.DestinationUrl = banner.DestinationUrl;
@@ -58,7 +65,7 @@ namespace Web.Controllers
                     model.DisplayImageUrl = banner.ImageUrl;
                 }
                 else
-                { 
+                {
                     return RedirectToAction("Index", "UserProfile").WithError("No tienes permiso para editar este Banner");
                 }
             }
@@ -68,32 +75,33 @@ namespace Web.Controllers
 
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Wizard(BannerViewModel model)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BannerViewModel model)
         {
             if (!ModelState.IsValid)
                 return View(model);
-            
+
             try
             {
                 var fileName = "";
 
-                if (HttpContext.Request.Form.Files != null && HttpContext.Request.Form.Files[0] != null)
+                if (HttpContext.Request.Form.Files?[0] != null)
                 {
                     fileName = UploadedFile(HttpContext.Request.Form.Files[0]);
                 }
 
-                if(model.Id > 0) 
+                if (model.Id > 0)
                 {
                     var bannerToUpdate = _bannersService.GetById(model.Id);
 
-                    if(bannerToUpdate.UserId == _currentUser.UserId)
+                    if (bannerToUpdate.UserId == _currentUser.UserId)
                     {
                         bannerToUpdate.DestinationUrl = model.DestinationUrl;
                         bannerToUpdate.ImageUrl = fileName;
 
                         var result = _bannersService.Update(bannerToUpdate);
 
-                        if(result.Success)
+                        if (result.Success)
                         {
                             return RedirectToAction("Index", "UserProfile").WithSuccess("Banner editado exitosamente");
                         }
@@ -101,10 +109,11 @@ namespace Web.Controllers
                         return View(model).WithError(result.Messages);
                     }
                     else
-                    { 
+                    {
                         return RedirectToAction("Index", "UserProfile").WithError("No tienes permiso para editar este Banner");
                     }
-                } else 
+                }
+                else
                 {
                     var banner = new Banner
                     {
@@ -115,7 +124,7 @@ namespace Web.Controllers
 
                     var result = _bannersService.Create(banner);
 
-                    if(result.Success)
+                    if (result.Success)
                     {
                         try
                         {
@@ -132,14 +141,13 @@ namespace Web.Controllers
                     }
 
                     return View(model).WithError(result.Messages);
-                }                  
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 HttpContext.RiseError(ex);
                 return View(model).WithError(ex.Message);
             }
-            
         }
 
         [Authorize]
@@ -152,52 +160,50 @@ namespace Web.Controllers
             {
                 var banner = _bannersService.GetById(id);
 
-                if(banner == null)
+                if (banner == null)
                 {
                     result.AddErrorMessage("No puedes eliminar una Banner que no existe.");
                 }
-                else if(banner.UserId != _currentUser.UserId)
+                else if (banner.UserId != _currentUser.UserId)
                 {
                     result.AddErrorMessage("No puedes eliminar un Banner que no creaste.");
                 }
-                
+
                 result = _bannersService.Delete(banner);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 HttpContext.RiseError(ex);
                 result.AddErrorMessage(ex.Message);
             }
-            
+
             return Json(result);
         }
 
-        private string UploadedFile(IFormFile file)  
-        {  
-            string uniqueFileName = null;  
-  
-            if (file != null)  
-            {  
-                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "img/banners");  
-                
-                uniqueFileName = $"{Guid.NewGuid().ToString()}_{file.FileName}";  
-                
-                string filePath = Path.Combine(uploadsFolder, uniqueFileName);  
-                
-                using (var fileStream = new FileStream(filePath, FileMode.Create))  
-                {  
-                    file.CopyTo(fileStream);  
-                }  
+        private string UploadedFile(IFormFile file)
+        {
+            string uniqueFileName = null;
+
+            if (file != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "img/banners");
+
+                uniqueFileName = $"{Guid.NewGuid().ToString()}_{file.FileName}";
+
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+
+                file.CopyTo(fileStream);
             }
 
-            return "https://emplea.do//img/banners/" + uniqueFileName;  
+            return "https://emplea.do//img/banners/" + uniqueFileName;
         }
 
-         /// <summary>
+        /// <summary>
         /// Validates the payload response that comes from the Slack interactive message actions
         /// </summary>
         /// <param name="payload"></param>
-        /// <returns></returns>s
         [HttpPost]
         [AllowAnonymous]
         public async Task Validate([FromForm] string payload)
@@ -242,6 +248,7 @@ namespace Web.Controllers
             catch (Exception ex)
             {
                 HttpContext.RiseError(ex);
+
                 if (ex.InnerException != null)
                     HttpContext.RiseError(ex.InnerException);
             }
